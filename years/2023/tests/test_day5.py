3d8f2f0aff43e1e7b21957e966961504dc936c41
loethor <lrb2392@gmail.com>
2023-12-14 21:54:21 +0100

    Added day 5 solutions

--- years/2023/tests/test_day5.py
+++ years/2023/tests/test_day5.py
@@ -1,37 +1,84 @@
-# import unittest
-# from days import day1
+import unittest
+from days import day5
+from days.day5 import (
+    Rules,
+    ReverseRules,
+    get_location,
+)
+from utils.utils import text_parser
 
-# def naive_parser(file_name: str) -> [str]:
-#     with open(file_name, 'r') as file:
-#         data = file.read().splitlines()
-#     return data
+
+class ExternalFunctions(unittest.TestCase):
+    def test_example_1(self):
+        input_data = text_parser("data/example_day5.txt")
+        expected_output = 35
+
+        self.assertEqual(day5.solveA(input_data), expected_output)
+
+    def test_part_1(self):
+        # Write test cases based on expected functionality
+        input_data = text_parser("data/day5.txt")
+        expected_output = 313045984
+
+        self.assertEqual(day5.solveA(input_data), expected_output)
+
+    def test_example_2(self):
+        # Write test cases based on expected functionality
+        input_data = text_parser("data/example_day5.txt")
+        expected_output = 46
+
+        self.assertEqual(day5.solveB(input_data), expected_output)
+
+    def test_part_2(self):
+        # Write test cases based on expected functionality
+        input_data = text_parser("data/day5.txt")
+        expected_output = 20283860
+
+        self.assertEqual(day5.solveB(input_data), expected_output)
 
 
-# class TestDay1(unittest.TestCase):
-#     def test_example_1(self):
-#         # Write test cases based on expected functionality
-#         input_data = naive_parser("data/example_day1A.txt")
-#         expected_output = 142
+class InternalFunctions(unittest.TestCase):
+    def test_rules(self):
+        input_str = "seed-to-soil map:\n50 98 2\n52 50 48"
+        expected_rules = [[50, 98, 2], [52, 50, 48]]
+        my_rules = Rules(input_str)
+        self.assertEqual(my_rules.rules, expected_rules)
 
-#         self.assertEqual(day1.solveA(input_data), expected_output)
+    def test_reverse_rules(self):
+        input_str = "seed-to-soil map:\n50 98 2\n52 50 48"
+        expected_rules = [[98, 50, 2], [50, 52, 48]]
+        my_rules = ReverseRules(input_str)
+        self.assertEqual(my_rules.rules, expected_rules)
 
-#     def test_part_1(self):
-#         # Write test cases based on expected functionality
-#         input_data = naive_parser("data/day1.txt")
-#         expected_output = 55488
+    def test_apply_rules_in_range(self):
+        input_str = "seed-to-soil map:\n50 98 2\n52 50 48"
+        my_rules = Rules(input_str)
+        expected_output = 50
+        self.assertEqual(my_rules.apply_rules(98), expected_output)
 
-#         self.assertEqual(day1.solveA(input_data), expected_output)
+    def test_apply_rules_out_range(self):
+        input_str = "seed-to-soil map:\n50 98 2\n52 50 48"
+        my_rules = Rules(input_str)
+        expected_output = 30
+        self.assertEqual(my_rules.apply_rules(30), expected_output)
 
-#     def test_example_2(self):
-#         # Write test cases based on expected functionality
-#         input_data = naive_parser("data/example_day1B.txt")
-#         expected_output = 281+77
+    def test_get_location(self):
+        seed_str = "seed-to-soil map:\n50 98 2"
+        soil_str = "whatever-to-soil map:\n30 50 2"
+        rules1 = Rules(seed_str)
+        rules2 = Rules(soil_str)
+        expected_output = 30
 
-#         self.assertEqual(day1.solveB(input_data), expected_output)
+        self.assertEqual(get_location(98, [rules1, rules2]), expected_output)
 
-#     def test_part_2(self):
-#         # Write test cases based on expected functionality
-#         input_data = naive_parser("data/day1.txt")
-#         expected_output = 0
+    def test_apply_reverse_rules_in_range(self):
+        input_str = "seed-to-soil map:\n50 98 2\n52 50 48"
+        my_rules = ReverseRules(input_str)
+        expected_output = 98
+        self.assertEqual(my_rules.apply_rules(50), expected_output)
 
-#         self.assertEqual(day1.solveB(input_data), expected_output)
+    def test_apply_rulesreverse__out_range(self):
+        input_str = "seed-to-soil map:\n50 98 2\n52 50 48"
+        my_rules = ReverseRules(input_str)
+        expected_output = 30
+        self.assertEqual(my_rules.apply_rules(30), expected_output)
--- years/2023/utils/utils.py
+++ years/2023/utils/utils.py
@@ -1,4 +1,17 @@
-def naive_parser(file_name: str) -> [str]:
+def list_string_parser(file_name: str) -> [str]:
     with open(file_name, "r") as file:
         data = file.read().splitlines()
     return data
+
+
+def text_parser(file_name: str) -> [str]:
+    with open(file_name, "r") as file:
+        data = file.read()
+    return data
+
+
+def block_parser(file_name: str) -> [str]:
+    with open(file_name, "r") as file:
+        data = file.read()
+        blocks = [block.strip() for block in data.split("\n\n") if block.strip()]
+        return blocks
