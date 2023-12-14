b73f452edc79d57fc8b1dbed1bf07aa6da37e119
loethor <lrb2392@gmail.com>
2023-12-14 23:05:34 +0100

    Added day 6

--- years/2023/days/day6.py
+++ years/2023/days/day6.py
@@ -1,6 +1,38 @@
-def solveA(input_data: [chr]) -> int:
-    return 0
+from typing import List
+from functools import reduce
 
 
-def solveB(input_data: [chr]) -> int:
-    return 0
+def solveA(input_data: List[str]) -> int:
+    times = parse_input(input_data[0])
+    distances = parse_input(input_data[1])
+
+    victories = []
+    for time, distance in zip(times, distances):
+        victories.append(obtain_number_of_victories(time, distance))
+
+    return reduce((lambda x, y: x * y), victories)
+
+
+def solveB(input_data: List[str]) -> int:
+    time = parse_input_bad_kerning(input_data[0])
+    distance = parse_input_bad_kerning(input_data[1])
+    return obtain_number_of_victories2(time, distance)
+
+
+def parse_input(input_data: str) -> List[int]:
+    _, numbers = input_data.split(":")
+    return list(map(int, numbers.strip().split()))
+
+
+def obtain_number_of_victories(time: int, distance: int) -> int:
+    victories = [1 for i in range(1, time) if (i * (time - i)) > distance]
+    return sum(victories)
+
+
+def obtain_number_of_victories2(time: int, distance: int) -> int:
+    return sum(1 for i in range(1, time) if (i * (time - i)) > distance)
+
+
+def parse_input_bad_kerning(input_data: str) -> int:
+    _, numbers = input_data.split(":")
+    return int(numbers.replace(" ", ""))
